c74eda4d1cb567154abc29947271b64f0668691c
Wataru Nishie <nishie@wataru-no-macbook-pro.local>
2016-10-30 15:38:21 +0900

    Initial Commit

---
+++ SwiftPiano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPiano
+//
+//  Created by Wataru Nishie on 2016/10/30.
+//  Copyright © 2016年 Wataru Nishie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
