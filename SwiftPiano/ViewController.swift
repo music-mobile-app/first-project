b20db2c072e078238381dd08194d930b04054d4d
music-mobile-app <music@nw-exam.com>
2016-11-06 16:39:54 +0900

    aif化

--- SwiftPiano/ViewController.swift
+++ SwiftPiano/ViewController.swift
@@ -12,31 +12,51 @@ import AVFoundation
 class ViewController: UIViewController {
 
     let pianoKeys = ["C3","C#","D","D#","E","F","F#","G","G#","A","A#","B","C4"]
-    var audioPlayers:[AVAudioPlayer] = []
+    var audioPlayersAif:[AVAudioPlayer] = []
+    var audioPlayersMp3:[AVAudioPlayer] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var audioPlayer:AVAudioPlayer!
         
         for key in pianoKeys {
-            let audioPath = Bundle.main.path(forResource: "sound/" + key, ofType:"mp3")!
-            let pianoSoundURL = URL(fileURLWithPath: audioPath)
-            
+            var path:String = "";
+            var type:String = "";
+            path = "sp_sounds/" + key
+            //-----------------------------------------------
+            // mp3
+            //-----------------------------------------------
+            type = "mp3"
+            var audioPath = Bundle.main.path(forResource: path, ofType:type)!
+            var pianoSoundURL = URL(fileURLWithPath: audioPath)
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: pianoSoundURL)
             } catch let error as NSError {
                 audioPlayer = nil
             }
             audioPlayer.prepareToPlay()
-            audioPlayers.append(audioPlayer)
+            audioPlayersMp3.append(audioPlayer)
+            //-----------------------------------------------
+            // aif
+            //-----------------------------------------------
+            type = "aif"
+            audioPath = Bundle.main.path(forResource: path, ofType:type)!
+            pianoSoundURL = URL(fileURLWithPath: audioPath)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: pianoSoundURL)
+            } catch let error as NSError {
+                audioPlayer = nil
+            }
+            audioPlayer.prepareToPlay()
+            audioPlayersAif.append(audioPlayer)
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func playkey(sender: UIButton){
         let keynum = sender.tag
-        audioPlayers[keynum].currentTime = 0
-        audioPlayers[keynum].play()
+        audioPlayersAif[keynum].currentTime = 0
+        audioPlayersAif[keynum].play()
     }
 
     override func didReceiveMemoryWarning() {
